1636470feb25055cb46073096172c8ba4b223fe5
Robin Pokorny <robin.pokorny@klarna.com>
2021-06-25 17:12:40 +0200

    :zap: second approach

--- src/core/index.js
+++ src/core/index.js
@@ -1,58 +1,112 @@
-import _ from 'lodash'
-import { files } from '../utils'
-import config from '../config'
+import _, { map } from 'lodash';
+import { files } from '../utils';
+import config from '../config';
 
-export const includes = (list, pair) => list.some(e => Array.isArray(e) && e.every((o, i) => Object.is(pair[i], o) || Object.is([...pair].reverse()[i], o)))
-export const findCombinations = arr => arr.map((v, i) => arr.slice(i + 1).map(w => [v, w])).flat()
+export const includes = (list, [a, b]) =>
+  list.some(([x, y]) => (a === x && b === y) || (a === y && b === x));
+export const findCombinations = (arr) =>
+  arr.map((v, i) => arr.slice(i + 1).map((w) => [v, w])).flat();
 
-export const arrange = (attendees, logs) => {
+export const arrange2 = (attendees, logs) => {
   const combinations = findCombinations(attendees)
-    .filter(i => !includes(logs.map(o=>o.pairs).flat(), i.map(v=>v.tag)))
-    .filter(i => i[0].team !== i[1].team)
-    .map(o => o.map(v => v.tag))
-  let list = []
-  let tempList = [...combinations]
-  let index = 0
-  while (list.length < Math.floor(attendees.length / 2) && index < attendees.length - 1) {
-    const tempEle = tempList.shift()
+    .filter(
+      (i) =>
+        !includes(
+          logs.map((o) => o.pairs).flat(),
+          i.map((v) => v.tag)
+        )
+    )
+    .filter((i) => i[0].team !== i[1].team)
+    .map((o) => o.map((v) => v.tag));
+  let list = [];
+  let tempList = [...combinations];
+  let index = 0;
+  while (
+    list.length < Math.floor(attendees.length / 2) &&
+    index < attendees.length - 1
+  ) {
+    const tempEle = tempList.shift();
     if (tempEle) {
-      list.push(tempEle)
-      tempList = tempList.filter(i => !i.includes(tempEle[0]) && !i.includes(tempEle[1]))
+      list.push(tempEle);
+      tempList = tempList.filter(
+        (i) => !i.includes(tempEle[0]) && !i.includes(tempEle[1])
+      );
     } else {
-      list = []
-      tempList = [...combinations]
-      tempList.splice(0, index++)
+      list = [];
+      tempList = [...combinations];
+      tempList.splice(0, index++);
     }
   }
-  return list
-}
+  return list;
+};
+
+const peopleMet = (list, tag) => {
+  const pairsWithTag = list.filter(([a, b]) => a === tag || b === tag).flat();
+
+  return new Set(pairsWithTag);
+};
+
+export const arrange = (attendees, logs) => {
+  const flatLogs = logs.flatMap(({ pairs }) => pairs);
+  const recent = attendees.reduce(
+    (map, { tag }) => map.set(tag, peopleMet(flatLogs, tag)),
+    new Map()
+  );
+
+  const teamNames = [...new Set(attendees.map(({ team }) => team))];
+
+  const teams = teamNames.reduce(
+    (map, name) =>
+      map.set(
+        name,
+        new Set(
+          attendees.filter(({ team }) => team === name).map(({ tag }) => tag)
+        )
+      ),
+    new Map()
+  );
+
+  return attendees.reduce((pairs, { tag, team }) => {
+    if (pairs.has(tag)) return pairs;
+
+    const next = attendees.find(
+      ({ tag: candidate }) =>
+        tag !== candidate &&
+        !pairs.has(candidate) &&
+        !recent.get(tag).has(candidate) &&
+        !teams.get(team).has(candidate)
+    );
+
+    if (!next) {
+      throw new Error(
+        'ERROR: unable to create a valid list, please reduce the log cap or retry'
+      );
+    }
+
+    return pairs.set(tag, next.tag).set(next.tag, tag);
+  }, new Map());
+};
 
 export const findPairs = () => {
-  const logs = files.loadLogs()
-  files.checkChangingLogs(logs)
-  const attendees = _.shuffle(files.loadAttendees())
+  const logs = files.loadLogs();
+  files.checkChangingLogs(logs);
+  const attendees = _.shuffle(files.loadAttendees());
 
-  console.log('Assembling the pair list... ')
+  console.log('Assembling the pair list... ');
 
-  const list = arrange(attendees, logs)
+  const list = arrange(attendees, logs);
 
-  console.log('\n')
-  console.table(list)
-
-  if (list.length < 1) {
-    console.error('ERROR: unable to create a valid list, please reduce the log cap or retry')
-    console.log('Aborting...')
-    return
-  }
+  console.log('\n');
+  console.table(list);
 
   logs.push({
     date: new Date().toLocaleDateString(),
-    pairs: list,
-  })
+    pairs: [...list],
+  });
 
   if (logs.length > config.LOGS_CAP) {
-    logs.shift()
+    logs.shift();
   }
 
-  files.promptToSave(list, logs)
-}
+  files.promptToSave(list, logs);
+};
--- src/utils/files.js
+++ src/utils/files.js
@@ -1,55 +1,61 @@
-import fs from 'fs'
-import readline from 'readline-sync'
-import config from '../config'
+import fs from 'fs';
+import readline from 'readline-sync';
+import config from '../config';
 
-const loadFile = path => {
+const loadFile = (path) => {
   try {
-    const data = fs.readFileSync(path, 'utf8')
-    return JSON.parse(data)
+    const data = fs.readFileSync(path, 'utf8');
+    return JSON.parse(data);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 const writeFile = (path, data) => {
   try {
-    fs.writeFileSync(path, JSON.stringify(data))
+    fs.writeFileSync(path, JSON.stringify(data));
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 
 export const loadAttendees = (path = config.DEFAULT_ATTENDEES_PATH) => {
-  console.log('Loading attendees...')
-  return loadFile(path)
-}
+  console.log('Loading attendees...');
+  return loadFile(path);
+};
 
 export const loadLogs = (path = config.DEFAULT_LOGS_PATH) => {
-  console.log('Loading logs...')
-  return loadFile(path)
-}
+  console.log('Loading logs...');
+  return loadFile(path);
+};
 
 const updateLogs = (logs, path = config.DEFAULT_LOGS_PATH) => {
-  writeFile(path, logs)
-}
+  writeFile(path, logs);
+};
 
-export const checkChangingLogs = logs => {
+export const checkChangingLogs = (logs) => {
   if (logs.length > config.LOGS_CAP) {
-    if (readline.keyInYN('It seems your log cap has been updated recently.\nDo you want to remove the oldest logs accordingly?')) {
-      logs = logs.slice(logs.length - config.LOGS_CAP)
-      updateLogs(logs)
-      console.log('Oldest logs removed')
+    if (
+      readline.keyInYN(
+        'It seems your log cap has been updated recently.\nDo you want to remove the oldest logs accordingly?'
+      )
+    ) {
+      logs = logs.slice(logs.length - config.LOGS_CAP);
+      updateLogs(logs);
+      console.log('Oldest logs removed');
     }
   }
-}
+};
 
 export const promptToSave = (list, logs) => {
   if (readline.keyInYN('Is the list fine? (Y)')) {
-    console.log('Saving the list...')
-    updateLogs(logs)
-    console.log('Please paste the following message in slack channel #card-random-fika\n')
-    list.map(e => console.log(`${e[0]} - ${e[1]}`))
+    console.log('Saving the list...');
+    updateLogs(logs);
+    console.log(
+      'Please paste the following message in slack channel #card-random-fika\n'
+    );
+    list.forEach((one, two) => console.log(`${one} - ${two}`));
   } else {
-    console.log('Aborting...')
+    console.log('Aborting...');
   }
-}
+};
